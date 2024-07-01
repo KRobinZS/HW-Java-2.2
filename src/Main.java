b1b8f4457449f736c3acf52acdc530597befdbe3
Fasakhova <kvisti2009@mail.ru>
2024-07-01 14:50:37 +0300

    Two commit

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,16 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int score = 100; // на счету у клиента
-        int replenish = 1500; // сумма пополнения
+        int Score = 100; // на счету у клиента
+        int Replenish = 1500; // сумма пополнения
         int bonus;
-        if (replenish > 1000) {
-            bonus = (score + replenish) / 100;
+        if (Replenish > 1000) {
+            bonus = (Score + Replenish) / 100;
         } else {
             bonus = 0;
         }
-        int final_score = score + replenish + bonus;
-        System.out.println("Ваш баланс:" + final_score);
+        int finalScore = Score + Replenish + bonus;
+        System.out.println("Ваш баланс:" + finalScore);
 
     }
 }
